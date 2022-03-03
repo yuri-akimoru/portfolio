96020e896e2d25205d798f06491be618761055a3
Yuri Akimoru <yuri.akimoru@outlook.com>
2022-03-02 18:24:22 -0800

    update

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Christpher Saburo Portfolio">
-  <link rel="icon" href="./favicon.png">
+  <link rel="icon" href="/assets/images/favicon.png">
   <link href='https://fonts.googleapis.com/css?family=Lato:300,400,300italic,400italic' rel='stylesheet'
     type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
